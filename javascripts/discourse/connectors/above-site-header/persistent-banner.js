5c13bba2a99746d613f466fe78868a816f6f32c6
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2024-09-24 18:48:37 +0800

    Update persistent-banner.js

--- javascripts/discourse/connectors/above-site-header/persistent-banner.js
+++ javascripts/discourse/connectors/above-site-header/persistent-banner.js
@@ -1,5 +1,6 @@
 import Component from "@glimmer/component";
 
+alert(settings.banner_text_content);
 export default class persistentbanner extends Component {
   get bannerIsFilled() {
     if (settings.banner_text_content == "") {
